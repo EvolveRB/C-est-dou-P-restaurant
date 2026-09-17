6fa651933e0ba81b5f3766c25a5b14df10955f64
EvolveRB <rbgreinaldo@gmail.com>
2026-09-17 15:49:03 -0300

    Estructura Backend, Frontend y bd con ejemplo

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,29 @@
-# pyrefly: ignore [missing-import]
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from database import SessionLocal, engine
+from models import Base, Producto, Mesa
+from sqlalchemy.orm import Session
 
-# Inicializamos la aplicación con información para la documentación automática
-app = FastAPI(
-    title="API Restaurante ",
-    description="API REST para sistema de autogestión de pedidos",
-    version="1.0.0"
-)
+# Opcional: Esto crea las tablas en la BD si no existen
+Base.metadata.create_all(bind=engine)
 
-@app.get("/")
-def ruta_raiz():
-    return {"mensaje": "Servicio Levantado"}
+app = FastAPI(title="API Restaurante")
 
-@app.get("/health")
-def health_check():
-    return {"status": "ok", "sistema": "operativo"}
+# Dependencia para obtener la sesión de base de datos en cada petición
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+# Ruta para ver todas las mesas
+@app.get("/mesas")
+def obtener_mesas(db: Session = Depends(get_db)):
+    mesas = db.query(Mesa).all()
+    return mesas
+
+# Ruta para ver todos los productos
+@app.get("/productos")
+def obtener_productos(db: Session = Depends(get_db)):
+    productos = db.query(Producto).all()
+    return productos
